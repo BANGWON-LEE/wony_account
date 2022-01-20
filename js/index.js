76a61dcc1b9ef4c04bef78f717a6036e6ba39559
BANGWON-LEE <qkddnjs31@gmail.com>
2022-01-20 20:41:39 +0900

    20220120 로그인 작업

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 function userLogin() {
  
-    console.log('시도 start');
+    // console.log('시도 start');
     let form = document.createElement('form');
     let mem_id = document.getElementById('login_id').value;
     let mem_pw = document.getElementById('login_pw').value;
@@ -8,7 +8,6 @@ function userLogin() {
     
     let info = [mem_id, mem_pw ];
 
-    alert('로그인 start');
     form.setAttribute('action', 'http://localhost:5001/api/login');
     form.setAttribute('method', 'get');
     document.characterSet="utf-8";
@@ -23,7 +22,7 @@ function userLogin() {
         form.appendChild(hiddenField);
     }
 
-    sessionStorage.setItem("users", JSON.stringify(info));
+    //sessionStorage.setItem("users", JSON.stringify(info));
     document.body.appendChild(form);
     form.submit();
     
